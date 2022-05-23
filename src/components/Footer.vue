7357fd7c8c8bf2fa645f29fc0a8a88a0d1d354d9
Maika <maika.tp.0331@gmail.com>
2022-05-24 02:35:45 +0900

    フッターにメンバーの名前を記載しました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, Sho Honma and ○○○</span>.
+      <span class="names">Maika Tsubaki, Sho Honma and Tatsuki Kueosawa</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
